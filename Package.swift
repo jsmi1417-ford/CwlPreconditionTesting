6834f3f0220390cd32bf8774fc332a34e85a79ad
jsmi1417-ford <39744622+jsmi1417-ford@users.noreply.github.com>
2021-09-21 01:47:02 -0400

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
 		.library(name: "CwlPosixPreconditionTesting", targets: ["CwlPosixPreconditionTesting"])
 	],
 	dependencies: [
-		.package(url: "https://github.com/mattgallagher/CwlCatchException.git", from: "2.0.0")
+		.package(url: "https://github.com/jsmi1417-ford/CwlCatchException.git", from: "2.0.0")
 	],
 	targets: [
 		.target(
